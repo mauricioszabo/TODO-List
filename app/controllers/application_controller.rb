7e9d10d8de826023ab3c3744e752584ea16976fb
Maurício Szabo <mauricio.szabo@gmail.com>
2010-08-20 18:35:48 -0300

    Started working on authentication (Devise).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,14 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  before_filter :login_user
 
   # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  filter_parameter_logging :password
+
+  def login_user
+    return if %w(sessions passwords registrations).include?(params[:controller])
+    return if user_signed_in?
+    redirect_to new_user_registration_path
+  end
 end
